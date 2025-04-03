dbcf85fd4511bdf9994f7128a524a6837c02665b
Amin Mohamadi <amindevzone@gmail.com>
2025-04-03 12:16:00 +0330

    df

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print('Ich hoffe, dass es euch gut geht')
 print('Ich lerne seit fast sechs Moneten Deutsch')
 
 print('Ich muss damit anfangen, Deutschsprechen zo lernen')
+print('Ich muss damit anfangen, Deutschsprechen zo lernen')
+print('Ich muss damit anfangen, Deutschsprechen zo lernen')
+print('Ich muss damit anfangen, Deutschsprechen zo lernen')
+print('Ich muss damit anfangen, Deutschsprechen zo lernen')
