3f4605884e3f227ab11959761de0df0223d81a03
Amin Mohamadi <amindevzone@gmail.com>
2025-04-05 12:27:42 +0330

    remove add function for better performance

--- utils.py
+++ utils.py
@@ -1,17 +1,3 @@
-def add (a, b):
-    """
-    add two numbers.
-
-    :param a: _description_
-    :type a: _type_
-    :param b: _description_
-    :type b: _type_
-    :return: _description_
-    :rtype: _type_
-    """
-    return a + b
-
-
 def sub (a, b):
     """
     subtract two numbers.
